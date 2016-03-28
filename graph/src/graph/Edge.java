f717b3aec47e69d5451f5ee5d329f5a03ea4d746
AgbodjogbeYves-alain <agboniel@yahoo.fr>
2016-03-28 14:48:58 +0200

    Ajout procedure reliersommets et supprimersommets

--- graph/src/graph/Edge.java
+++ graph/src/graph/Edge.java
@@ -6,6 +6,39 @@ public class Edge {
 	int numEdge;
 	Vertex[] relatedVertex = new Vertex[2];
 	
+	public Edge(int num){
+		numEdge = num;
+		relatedVertex = null;
+	}
 	
+	public void relierSommet(Vertex v,Vertex x)
+	{
+		if (relatedVertex[0]== null || relatedVertex[1] == null){
+		relatedVertex[0] = v;
+		relatedVertex[1] = x;
+		}
+		else System.out.println("Cette arrete est deja lié a 2");
+	}
 
+	public int getEdge()
+	{
+		return this.numEdge;
+	}
+	
+	public void setEdge(int num)
+	{
+		this.numEdge = num;
+	}
+	
+	public void supprimerRelation()
+	{
+		if (relatedVertex[1] != null){
+		this.relatedVertex[1].relatedEdge.remove(numEdge);
+		this.relatedVertex[1].relatedVertex.remove(relatedVertex[2]);
+		this.relatedVertex[2].relatedEdge.remove(numEdge);
+		this.relatedVertex[2].relatedVertex.remove(relatedVertex[1]);
+		this.relatedVertex = null;
+		}
+		else {this.relatedVertex = null;}
+	}
 }
