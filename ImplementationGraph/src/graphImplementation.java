8fc31169c93cb8a775d7577ac98c852fd95aec1f
AgbodjogbeYves-alain <agboniel@yahoo.fr>
2016-04-04 14:59:17 +0200

    Premiere implementation de graph

--- ImplementationGraph/src/graphImplementation.java
+++ ImplementationGraph/src/graphImplementation.java
@@ -5,6 +5,7 @@ import bin.Graphe;
 public class graphImplementation implements Graphe {
 
 	Vertex[][] tabvertex = new Vertex[10][10];
+	Edge[][] tabEdge = new Edge[10][10];
 
 	@Override
 	public void addEdge(Vertex arg0, Vertex arg1) {
@@ -14,14 +15,30 @@ public class graphImplementation implements Graphe {
 
 	@Override
 	public void addVertex(Vertex arg0) {
+		int i = 0;
+		int y = 0;
+		boolean insere = false;
+		for (i=0;i<10;i++){
+			for (y=0;y<10;y++){
+				if (this.tabvertex[i][y] == null){
+					this.tabvertex[i][y]= arg0;
+					insere = true;
+				}
 		
-		
+			}
+		}
+		if (insere  == false){
+			System.out.println("Tableau de vertex plein" );
+		}
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
+		if(this.tabvertex == null && this.tabEdge ==null){
+			return true;
+		}
 		return false;
+		
 	}
 
 	@Override
